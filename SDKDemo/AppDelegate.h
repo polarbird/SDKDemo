a14f0e7aae5caaa6950b42951ef4e8e7900e715e
polarbird <polarbird@polarbirddeMac.local>
2020-07-11 14:15:52 +0800

    init project

---
+++ SDKDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YoseenDemo
+//
+//  Created by polarbird on 2020/7/4.
+//  Copyright © 2020 YoseenIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SDKDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YoseenDemo
+//
+//  Created by polarbird on 2020/7/4.
+//  Copyright © 2020 YoseenIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
